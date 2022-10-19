a397627d6bb44286525356f0c21043abbe031ace
KritianWhite <3617836980101@ingenieria.usac.edu.gt>
2022-10-18 23:13:18 -0600

    Sesion 8, enviar de un datos de un hijo a un padre :+1:

--- hola-mundo/src/App.js
+++ hola-mundo/src/App.js
@@ -8,6 +8,7 @@ import Ejemplo2 from './hooks/Ejemplo2';
 import MiComponenteConContexto from './hooks/Ejemplo3';
 import Ejemplo4 from './hooks/Ejemplo4';
 import GreetingStyled from './Components/pure/greetingStyled';
+import Father from './Components/container/father';
 
 
 function App() {
@@ -15,15 +16,19 @@ function App() {
     <div className="App">
       {/* <header className="App-header"> */}
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
         {/* Componente propio Greeting.jsx */}
         {/*<Greeting name="Christian :D"></Greeting>*/}
+
         {/** Componente de ejemplo funcional */}
         {/*<GreetingF name="Christian"></GreetingF>*/}
+
         {/**Componente del listado de tareas */}
         {/*<TaskListComponent></TaskListComponent>*/}
         {/*<Ejemplo1></Ejemplo1>*/}
         {/*<Ejemplo2></Ejemplo2>*/}
         {/*<MiComponenteConContexto></MiComponenteConContexto>*/}
+
         {/*<Ejemplo4 name='Christian'>*/}
           {/**Todo lo que hay aqui adentro es tratado como props.children */}
           {/*<h3>*/}
@@ -34,7 +39,12 @@ function App() {
         {/*<GreetingStyled name="Christian"></GreetingStyled>*/}
       {/* </header> */}
 
+      {/**Gestion de eventos */}
       <TaskListComponent></TaskListComponent>
+
+      {/**Enviando desde el hijo al padre */}
+      {/* {<Father></Father>} */}
+
 
     </div>
   );
